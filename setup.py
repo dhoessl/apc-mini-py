3aa243afda12efb635d0dc5b18f38570af992d99
Dominic Hößl <dominic.hoessl@noris.de>
2025-02-18 13:14:26 +0100

    add myself as dev and bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='akai_pro_py',
-    version='0.2.3',
+    version='0.3.1',
     description='A simple library for interfacing with the Akai Professional series of MIDI controllers',
-    url='https://github.com/Secretowo/apc-mini-py',
-    author='Secret and Lewis L. Foster',
-    author_email='Secret <unknown>, Lewis L. Foster <lewis@sniff122.tech>',
+    url='https://github.com/dhoessl/apc-mini-py',
+    author='Secret and Lewis L. Foster, Dominic Hößl',
+    author_email='Secret <unknown>, Lewis L. Foster <lewis@sniff122.tech>, Dominic Hößl <dominichoessl@gmail.com>',
     license='',
     packages=['akai_pro_py'],
     install_requires=['mido',
